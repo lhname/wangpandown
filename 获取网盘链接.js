33bad16fac09d2b6e35cb8b8e016ef92827c48bc
LH <linhaicptu@gmail.com>
2023-07-20 12:58:36 +0800

    add xywp1 domain

--- 获取网盘链接.js
+++ 获取网盘链接.js
@@ -23,7 +23,7 @@
 // @connect      127.0.0.1
 // @connect      192.168.1.38
 // @connect      localhost
-// @connect      http://www.xingyaopan.com
+// @connect      http://www.xingyaopan.com/*
 // @license      GPL-3.0-or-later
 // ==/UserScript==
 
